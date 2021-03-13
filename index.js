feb8c0826202401488438c074bdfd7ddfbdfeed7
Alex Cline <rev.a.r.cline@gmail.com>
2021-03-13 17:55:49 -0600

    commit tree f81e0944903795976e44bf7b998785ec5a496727

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(name) {
+  drivers.push(name);
+}
+
+function destructivelyPrependDriver(name) {
+  drivers.unshift(name);
+}
+
+function destructivelyRemoveLastDriver() {
+  drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver() {
+  drivers.shift();
+}
+
+function appendDriver(name) {
+  const drivers_app = drivers.concat(name);
+  return drivers_app;
+}
+
+function prependDriver(name) {
+  const drivers_prep = [name, ...drivers];
+  return drivers_prep;
+}
+
+function removeLastDriver() {
+  const drivers_last = drivers.slice(0, -1);
+  return drivers_last;
+}
+
+function removeFirstDriver() {
+  const drivers_first = drivers.slice(1);
+  return drivers_first;
+}
